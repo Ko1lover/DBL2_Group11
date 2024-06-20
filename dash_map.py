8e1ba52c012c3a4ea8f9e8e7939e6fa1f6caf79c
ias101 <101855032+ias101@users.noreply.github.com>
2024-06-21 02:09:57 +0800

    Update dash_map.py

--- dash_map.py
+++ dash_map.py
@@ -5,13 +5,23 @@ import dash_leaflet as dl
 import pandas as pd
 from dash import dash_table
 
+# download boundray file:
+# https://data.london.gov.uk/download/london_boroughs/9502cdec-5df0-46e3-8aa1-2b5c5233a31f/London_Boroughs.gpkg
+
+import geopandas as gpd
+
+gdf = gpd.read_file("London_Boroughs.gpkg")
+
+gdf = gdf.to_crs(epsg=4326)
+
+gdf.to_file("Boroughs_boundaries.geojson", driver="GeoJSON")
 
 PAS_borough = pd.read_csv('assets/FINAL_agg_Dataset.csv')
 PAS_borough['Date'] = pd.to_datetime(PAS_borough['year'].astype(str) + 'Q' + PAS_borough['quarter'].astype(str))
 PAS_borough = PAS_borough.drop(columns=['year', 'quarter'])
-PAS_borough = PAS_borough[['Date','borough','GoodJoblocal','TrustMPS',
-'violent_q_crimes','property_q_crimes','public_order_q_crimes','drug_or_weapon_q_crimes',
-'other_q_crimes']]
+PAS_borough = PAS_borough[['Date', 'borough', 'GoodJoblocal', 'TrustMPS',
+                           'violent_q_crimes', 'property_q_crimes', 'public_order_q_crimes', 'drug_or_weapon_q_crimes',
+                           'other_q_crimes']]
 PAS_borough['stop_search_data'] = '...'
 PAS_borough['street_crime_data'] = '...'
 PAS_borough['PAS_data'] = '...'
@@ -38,7 +48,7 @@ app.layout = html.Div([
                 [
                     dl.BaseLayer(dl.TileLayer(), name='Base', checked=True),
                     dl.Overlay(
-                        dl.GeoJSON(url='assets/Boroughs_boundaries.geojson', id='borough-geojson',
+                        dl.GeoJSON(url='Boroughs_boundaries.geojson', id='borough-geojson',
                                    zoomToBounds=True,
                                    hoverStyle=dict(weight=5, color='#666', dashArray='')),
                         name='Borough',
@@ -55,6 +65,7 @@ app.layout = html.Div([
     )
 ])
 
+
 @app.callback(
     Output('selected-data-borough', 'columns'),
     Output('selected-data-borough', 'data'),
@@ -64,10 +75,12 @@ def update_selected_data(borough_feature, selected_quarter):
     if borough_feature is not None and selected_quarter is not None:
         borough = borough_feature['properties'].get('name', 'N/A')
         selected_quarter = pd.Period(selected_quarter, freq='Q')
-        borough_data = PAS_borough[(PAS_borough['borough'] == borough) & (PAS_borough['Date'].dt.to_period('Q') == selected_quarter)]
+        borough_data = PAS_borough[
+            (PAS_borough['borough'] == borough) & (PAS_borough['Date'].dt.to_period('Q') == selected_quarter)]
         if not borough_data.empty:
             return [{"name": i, "id": i} for i in borough_data.columns], borough_data.to_dict('records')
     return [], []
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
